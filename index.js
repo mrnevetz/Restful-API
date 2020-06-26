83be992e00df9b6c8f7724b65f9b9478b226551c
Steve Peterson <steven.peterson.16@us.af.mil>
2020-06-26 09:59:46 -0600

    Leaving for funeral

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res) {
+    res.send('Hello World! This is the Home Page.  Try /students');
+});
+
+app.get('/students', (req, res) => res.send('This will be a list of students'))
+
+app.get('/grades', (req, res) => res.send('This page will show the grades of a particular user'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
