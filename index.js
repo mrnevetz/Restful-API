a791e6f47ecf829e4ad2bacbf0fde1fa9bf39aa5
Steve Peterson <steven.peterson.16@us.af.mil>
2020-06-29 15:22:06 -0600

    Express API Done

--- index.js
+++ index.js
@@ -2,12 +2,46 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', function(req, res) {
-    res.send('Hello World! This is the Home Page.  Try /students');
+const students = [
+    { id: 1, name: 'Steve Peterson' },
+    { id: 2, name: 'John Doe' },
+    { id: 3, name: 'Jane Doe' },
+    { id: 4, name: 'Donald Trump' },
+    { id: 5, name: 'George Washington' }
+];
+
+const grades = [
+    { id: 1, grade: 'A' },
+    { id: 2, grade: 'A' },
+    { id: 3, grade: 'B' },
+    { id: 1, grade: 'C' }
+]
+
+app.get('/', (req, res) => {
+    res.send("'Hello World! This is the Home Page.<br /><br /><A HREF='/students'>Student List</a>");
 });
 
-app.get('/students', (req, res) => res.send('This will be a list of students'))
+app.get('/students', (req, res) => {
+    res.send(students);
+});
 
-app.get('/grades', (req, res) => res.send('This page will show the grades of a particular user'))
+app.get('/students/:id', (req, res) => {
+    id = req.params.id;
+    res.send("<b>Student Details</b><br />Id: "+id +"<br />Name: "+students[id]);
+});
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get('/grades/:id', (req, res) => {
+    const idGrades = grades.find(x => x.id === parseInt(req.params.id));
+    if (!idGrades) res.status(400).send('No grades found for this student');
+    res.send(idGrades);
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+
+
+
+//app.get()
+//app.post()
+//app.put()
+//app.delete()
